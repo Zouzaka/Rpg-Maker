b37943053d26182fb5150790148cb3aed78556bb
Zouzaka <hamza_annaba1@hotmail.com>
2015-05-23 18:45:10 +0100

    Create package.rb

---
+++ Pierres-De-Stats/package.rb
@@ -0,0 +1,8 @@
+# Package description
+Package.new(
+	name: 'pierres-de-stats',
+	version: vsn(1, 0, 0),
+	authors: {'Zouzaka' => ''},
+	components: ['script.rb'],
+	description: 'Changer les Stats grace a un objet specifique'
+)
