e2e770756d1740f3e79f88db1d4c7c5a6927cb22
Zouzaka <hamza_annaba1@hotmail.com>
2015-05-23 19:09:34 +0100

    Create package.rb

---
+++ Capture-De-Monstres/package.rb
@@ -0,0 +1,8 @@
+# Package description
+Package.new(
+	name: 'zouzaka-capture-de-monsters',
+	version: vsn(1, 0, 0),
+	authors: {'Zouzaka' => ''},
+	components: ['script.rb'],
+	description: 'Permet de Capturer les divers monstres'
+)
